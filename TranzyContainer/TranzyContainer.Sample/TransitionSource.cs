7b9725ef8c0d1e926a832447c0dac2efa1644feb
Diogo Nunes <d13nunes@gmail.com>
2017-10-14 12:40:47 +0100

    Sample project added

---
+++ TranzyContainer/TranzyContainer.Sample/TransitionSource.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Foundation;
+using UIKit;
+
+namespace TranzyContainer.Sample
+{
+    internal class TransitionSource : UITableViewSource
+    {
+
+        static int Counter = 0;
+
+        private readonly string CellIdentifier = "Identifier";
+        public ITransitionAnimation[] Items
+        {
+            get;
+            set;
+        }
+        public UIColor Color1 { get; private set; } = UIColor.FromRGB(0xff, 0x64, 0x5c);
+        public UIColor Color2 { get; private set; } = UIColor.FromRGB(0xa0, 0xff, 0x5c);
+
+        public TransitionSource()
+        {
+            Items = new ITransitionAnimation[]  {
+                Transition.Fade,
+                Transition.FromTop,
+                Transition.FromBottom,
+                Transition.FromLeft,
+                Transition.FromRight,
+                Transition.Scale,
+                Transition.None,
+            };
+            Counter += 1;
+            Counter %= 2;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return Items.Length;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            var cell = tableView.DequeueReusableCell(CellIdentifier);
+            if (cell == null)
+            {
+                cell = new UITableViewCell(UITableViewCellStyle.Default, CellIdentifier);
+            }
+            var animation = Items[indexPath.Row];
+            cell.TextLabel.Text = animation.GetType().Name;
+
+            cell.ContentView.BackgroundColor = indexPath.Row % 2 == Counter ? Color1 : Color2;
+
+            return cell;
+        }
+
+        public override nfloat GetHeightForRow(UITableView tableView, NSIndexPath indexPath)
+        {
+            return 55;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            var aDelegate = UIApplication.SharedApplication.Delegate as AppDelegate;
+            var transition = Items[indexPath.Row];
+            aDelegate.MainViewController.ReplaceViewControllerAsync(new TransitionsViewController(), transition).ConfigureAwait(false);
+        }
+
+
+    }
+}
---
+++ TranzyContainer/TranzyContainer.Sample/TransitionsViewController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using UIKit;
+
+namespace TranzyContainer.Sample
+{
+    public partial class TransitionsViewController : UIViewController
+    {
+        static int Counter = 0;
+
+        public UITableView _tableView;
+
+        public UIColor Color1 { get; private set; } = UIColor.FromRGB(0xff, 0x5c, 0xf1);
+        public UIColor Color2 { get; private set; } = UIColor.FromRGB(0x5c, 0xe4, 0xff);
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            Counter += 1;
+            Counter %= 2;
+
+            _tableView = new UITableView(View.Bounds)
+            {
+                Source = new TransitionSource(),
+                BackgroundView = null,
+                Opaque = false,
+                BackgroundColor = Counter % 2 == 0 ? Color1 : Color2,
+                SeparatorStyle = UITableViewCellSeparatorStyle.None,
+            };
+
+
+            View.AddSubview(_tableView);
+        }
+
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            _tableView.Frame = View.Bounds;
+        }
+    }
+}
+
